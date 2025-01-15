da2e168ca1a6bb8d7e947a6e3f58a3d8fe53f5e1
leopold yang <1480335580@qq.com>
2025-01-15 19:59:56 +0800

    train.py

--- train.py
+++ train.py
@@ -1,27 +1,19 @@
 from ultralytics import YOLO
-import argparse
+import yaml
 
 def train(args):
-    model = YOLO(args.weights)
+    model = YOLO(args['weights'])
     results = model.train(
-                    data=args.data, 
-                    epochs=args.epochs, 
-                    batch=args.batch, 
-                    imgsz=args.imgsz, 
-                    name=args.name,
-                    resume=args.resume
-                    )
+            data=args['data'], 
+            epochs=args['epochs'], 
+            batch=args['batch'], 
+            imgsz=args['imgsz'], 
+            name=args['name'],
+            resume=args['resume'],
+            workers=args['workers'],
+    )
 
 
 if __name__ == '__main__':
-    args = argparse.ArgumentParser()
-    args.add_argument('--epochs', type=int, default=50)
-    args.add_argument('--batch', type=int, default=16)
-    args.add_argument('--imgsz', type=int)
-    args.add_argument('--data', type=str, default='config.yaml')
-    args.add_argument('--weights', type=str, default='yolov8n.yaml', help='path to pretrained weights if training from pretrained')
-    args.add_argument('--name', type=str)
-    args.add_argument('--resume', type=bool, default=False)
-    args = args.parse_args()
-
-    train(args)
+    cfg = yaml.load(open(r'./cfgs/train.yaml', 'r'), Loader=yaml.FullLoader)
+    train(cfg)
